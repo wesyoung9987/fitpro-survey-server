740aaf7b643e949b164d5ae0dc61122b726766ab
Wes Young <wes.young14@gmail.com>
2017-03-12 13:42:01 -0600

    added to server.js

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ app.listen(port);
 
 console.log('app listening on', port);
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
 app.get('/users', function(req, res, next) {
     // User.find(function(err, users){
     //     res.send(users);
